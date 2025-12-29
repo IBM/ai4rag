cd34cacc48d1754adb0aad2bb5810d1676cbf1ff
Jakub Walaszczyk <kubamaxin@gmail.com>
2025-12-29 09:24:39 +0100

    Remove merge artifacts
    Signed-off-by: Jakub Walaszczyk <kubamaxin@gmail.com>

--- ai4rag/rag/template/rag_template.py
+++ ai4rag/rag/template/rag_template.py
@@ -2,13 +2,9 @@
 # Copyright IBM Corp. 2025
 # SPDX-License-Identifier: Apache-2.0
 # -----------------------------------------------------------------------------
-<<<<<<<< HEAD:ai4rag/rag/template/rag_template.py
-
 from abc import ABC
 
 
 class BaseTemplate(ABC):
     def __init__(self):
         pass
-========
->>>>>>>> 31d4ddf (Introduce base RAG components and update pre-selector (#2)):ai4rag/rag/retrieval/__init__.py
