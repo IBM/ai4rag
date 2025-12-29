2f5de55c881f32194e4604979f732b9a56c56108
MichalSteczko <michalo.steczko@gmail.com>
2025-12-29 09:30:39 +0100

    Add parametrized vector store and foundation model support (#3)
    Signed-off-by: Jakub Walaszczyk <kubamaxin@gmail.com>
    Signed-off-by: Michał Steczko <Michal.Steczko@ibm.com>
    Co-authored-by: Jakub Walaszczyk <kubamaxin@gmail.com>
    Co-authored-by: Michał Steczko <Michal.Steczko@ibm.com>

--- ai4rag/rag/embedding/llama_stack.py
+++ ai4rag/rag/embedding/llama_stack.py
@@ -2,16 +2,42 @@
 # Copyright IBM Corp. 2025
 # SPDX-License-Identifier: Apache-2.0
 # -----------------------------------------------------------------------------
-from typing import Any
+from llama_stack_client import Client
+
+from .base_model import BaseEmbeddingModel
+from .embedding_types import LLamaStackEmbeddingParams
+
+__all__ = ["LLamaStackEmbeddingModel"]
 
 
-class LlamaStackEmbeddingModel:
-    def __init__(self, model_id: str, params: dict[str, Any]):
-        self.model_id = model_id
-        self.params = params
+class LLamaStackEmbeddingModel(BaseEmbeddingModel[Client, LLamaStackEmbeddingParams]):
+    """Creates embeddings for LLamaStack client."""
+
+    def __init__(self, client: Client, model_id: str, params: LLamaStackEmbeddingParams):
+        super().__init__(client=client, model_id=model_id, params=params)
+
+    def _embed_text(self, text_input: list[str] | str) -> list[list[float]]:
+        """Embeds documents.
+
+        Parameters
+        ----------
+        text_input : list[str] | str
+            List of text-like chunks or single text-like chunk.
+
+        Returns
+        -------
+        list[list[float]]
+            Embeddings made from the list of texts or a single text.
+        """
+
+        return [
+            data.embedding
+            for data in self.client.embeddings.create(input=text_input, model=self.model_id, **self.params).data
+            if not isinstance(data.embedding, str)
+        ]
 
     def embed_documents(self, texts: list[str]) -> list[list[float]]:
-        """Embed documents.
+        """Embeds given list of strings.
 
         Parameters
         ----------
@@ -23,19 +49,19 @@ class LlamaStackEmbeddingModel:
         list[list[float]]
             Embeddings made from the list of texts.
         """
-        raise NotImplementedError()
+        return self._embed_text(text_input=texts)
 
     def embed_query(self, query: str) -> list[float]:
-        """Embed query text
+        """Embeds given query.
 
         Parameters
         ----------
         query : str
-            User's query as text.
+            Single text-like chunk.
 
         Returns
         -------
-        list[float]
-            Single embeddings vector made from the query.
+        list[]
+            Embeddings made from a single text.
         """
-        raise NotImplementedError
+        return self._embed_text(text_input=query)[0]
