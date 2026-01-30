54c95d1db9daa462c8e068ac4f7673a5518e503e
Jakub Walaszczyk <jwalaszc@redhat.com>
2026-01-30 15:38:30 +0100

    Update retriever import
    Signed-off-by: Jakub Walaszczyk <jwalaszc@redhat.com>

--- ai4rag/rag/retrieval/__init__.py
+++ ai4rag/rag/retrieval/__init__.py
@@ -2,3 +2,5 @@
 # Copyright IBM Corp. 2025-2026
 # SPDX-License-Identifier: Apache-2.0
 # -----------------------------------------------------------------------------
+
+from .retriever import Retriever
